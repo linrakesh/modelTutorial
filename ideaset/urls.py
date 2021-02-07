c6d60397d7450c1bdf076c4542792bbfe8616e3a
rakesh kumar <rakesh@binarynote.com>
2021-02-07 22:00:48 +0530

    basic app structure

--- ideaset/urls.py
+++ ideaset/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index,edit,remove
 
 urlpatterns = [
    path('', index , name='index'),
+   path('edit/<int:id>/', edit , name='edit'),
+   path('remove/<int:id>/',remove , name='remove'),
 
 ]
--- ideaset/views.py
+++ ideaset/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Task
 # Create your views here.
 
 
 def index(request):
-  return render(request, 'ideaset/index.html')
+  if request.user.is_authenticated:
+    current_user = request.user
+    tasks = Task.objects.all().filter(user__username=current_user.username).order_by('task_priority')
+    return render(request, 'ideaset/index.html',{'task_list':tasks})
+  else:
+    return render(request, 'ideaset/index-blank.html')
+
+
+def edit(request,id):
+  task = Task.objects.get(id=id)
+  return render(request,'ideaset/edit.html',{'task':task})
+
+def remove(request,id):
+  task = Task.objects.get(id=id)
+  task.delete()
+  return redirect('/')
