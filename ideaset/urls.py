fc72acee68b5d92f6ed4bfcc8bf8a176c850630b
rakesh kumar <rakesh@binarynote.com>
2021-02-07 22:59:31 +0530

    task completed

--- ideaset/urls.py
+++ ideaset/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index,edit,remove
+from .views import index,edit,remove,add
 
 urlpatterns = [
    path('', index , name='index'),
+   path('add/', add , name='add'),
    path('edit/<int:id>/', edit , name='edit'),
    path('remove/<int:id>/',remove , name='remove'),
 
--- ideaset/views.py
+++ ideaset/views.py
@@ -7,14 +7,41 @@ def index(request):
   if request.user.is_authenticated:
     current_user = request.user
     tasks = Task.objects.all().filter(user__username=current_user.username).order_by('task_priority')
-    return render(request, 'ideaset/index.html',{'task_list':tasks})
+    print(tasks.count())
+    if tasks.count()>=1:
+      return render(request, 'ideaset/index.html',{'task_list':tasks})
+    else:
+      return render(request,'ideaset/add_task.html')
   else:
     return render(request, 'ideaset/index-blank.html')
 
 
+def add(request):
+  if request.method == 'POST':
+    current_user = request.user
+    task_name = request.POST['task_name']
+    task_desc = request.POST['task_desc']
+    task_date = request.POST['task_date']
+    task_priority = request.POST['task_priority']
+    task = Task(task_name=task_name,task_desc=task_desc,task_date=task_date,task_priority = task_priority,user=current_user) 
+    task.save()
+    return redirect('/')
+  else:
+    return render(request,'ideaset/add_task.html')
+
+
 def edit(request,id):
   task = Task.objects.get(id=id)
-  return render(request,'ideaset/edit.html',{'task':task})
+  if request.method =='POST':
+    task.task_name = request.POST['task_name']
+    task.task_desc = request.POST['task_desc']
+    task.task_date = request.POST['task_date']
+    task.task_priority = request.POST['task_priority']
+    task.save()
+    return redirect('/')
+  else:
+    return render(request,'ideaset/edit.html',{'task':task})
+
 
 def remove(request,id):
   task = Task.objects.get(id=id)
