d232b398596cdcdd75d846c47046ca1edcc27d23
rakesh kumar <rakesh@binarynote.com>
2021-02-08 21:49:11 +0530

    login required added

--- Tutorial/urls.py
+++ Tutorial/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', include('django.contrib.flatpages.urls')),
+    path('pages/', include('django.contrib.flatpages.urls')),
     path('admin/', admin.site.urls),
     path('', include('ideaset.urls')),
     
