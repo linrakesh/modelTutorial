db78006ca86b6aaf42b740c95bf5ef51a2229355
rakesh kumar <rakesh@binarynote.com>
2021-02-09 21:04:46 +0530

    login logout added

--- Tutorial/urls.py
+++ Tutorial/urls.py
@@ -17,12 +17,17 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView,LogoutView 
 
 
 urlpatterns = [
-    path('pages/', include('django.contrib.flatpages.urls')),
+    
     path('admin/', admin.site.urls),
     path('', include('ideaset.urls')),
+    path('login/',LoginView.as_view(),name='login'),
+    path('logout/',LogoutView.as_view(),name='logout'),
+    path('pages/', include('django.contrib.flatpages.urls')),
+
     
 
 ]
